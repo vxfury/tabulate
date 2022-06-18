e88307040419c9f5ee9ed9490ea835dfc3887c23
Kiran <kiran.nowak@gmail.com>
2022-06-18 23:22:27 +0800

    update

--- profiler.h
+++ profiler.h
@@ -72,163 +72,6 @@ inline void DoNotOptimize(Tp &value)
         prefix##stddev = static_cast<double>(sqrt(__t));                                                      \
     } while (0)
 
-class ProfilerCache {
-  public:
-    static ProfilerCache &Instance()
-    {
-        static ProfilerCache instance;
-        return instance;
-    }
-
-    void insert(const std::string &name, double avg, double stddev)
-    {
-        collections.emplace_back(name, avg, stddev);
-    }
-
-    void mark_as_ref(const std::string &s)
-    {
-        auto it = std::find_if(collections.begin(), collections.end(), [&](const ProfileData &another) {
-            return s == another.name;
-        });
-        if (it != collections.end()) {
-            references.emplace_back(it->name, it->avg, it->stddev);
-            collections.erase(it);
-        } else {
-            std::cout << "not found" << std::endl;
-        }
-    }
-
-    void set_title(std::string title)
-    {
-        this->title = std::move(title);
-    }
-
-  private:
-    struct ProfileData {
-        double avg;
-        double stddev;
-        std::string name;
-        std::vector<std::string> keys;
-        ProfileData(const std::string &name_, double avg_, double stddev_) : avg(avg_), stddev(stddev_), name(name_)
-        {
-            std::regex pattern("([\\w\\-]+)");
-            auto key_end = std::sregex_iterator();
-            auto key_begin = std::sregex_iterator(name_.begin(), name_.end(), pattern);
-
-            for (auto it = key_begin; it != key_end; ++it) {
-                keys.push_back(it->str());
-            }
-        }
-    };
-    std::string title;
-    std::vector<ProfileData> references;
-    std::vector<ProfileData> collections;
-
-    void make_summary_table(tabulate::Table &table)
-    {
-        if (references.size() == 0) {
-            table.add("description", "average time(nanoseconds)", "coefficient of variation");
-        } else {
-            table.add("description", "average time", "average time(reference)", "coefficient of variation",
-                      "perfermance ratio");
-        }
-
-        int i = 0;
-        for (auto const &v : collections) {
-            if (references.size() != 0) {
-                auto ref = std::find_if(references.begin(), references.end(), [&](const ProfileData &another) -> bool {
-                    return v.keys[0] == another.keys[0];
-                });
-                if (ref != references.end()) {
-                    double diff = ref->avg / v.avg;
-
-                    std::string name = v.keys[0] + "(";
-                    if (v.keys.size() > 2) {
-                        name += "(";
-                    }
-                    for (size_t _i = 1; _i < v.keys.size(); _i++) {
-                        if (_i == v.keys.size() - 1) {
-                            name += v.keys[_i];
-                        } else {
-                            name += v.keys[_i] + ", ";
-                        }
-                    }
-                    if (v.keys.size() > 2) {
-                        name += ")";
-                    }
-                    name += " vs ";
-                    if (ref->keys.size() > 2) {
-                        name += "(";
-                    }
-                    for (size_t _i = 1; _i < ref->keys.size(); _i++) {
-                        if (_i == ref->keys.size() - 1) {
-                            name += ref->keys[_i];
-                        } else {
-                            name += ref->keys[_i] + ", ";
-                        }
-                    }
-                    if (ref->keys.size() > 2) {
-                        name += ")";
-                    }
-                    name += ")";
-                    table.add(name, v.avg, ref->avg, v.stddev / v.avg, diff);
-
-                    i++;
-                    if (diff >= 1.2) {
-                        table[i][4].format().color(tabulate::Color::green);
-                        if (diff >= 2.0) {
-                            table[i][4].format().styles(tabulate::Style::bold);
-                            if (diff >= 5.0) {
-                                table[i][4].format().styles(tabulate::Style::blink);
-                            }
-                        }
-                    } else if (diff <= 0.8) {
-                        table[i][4].format().color(tabulate::Color::red);
-                        if (diff <= 0.5) {
-                            table[i][4].format().styles(tabulate::Style::bold);
-                            if (diff <= 0.2) {
-                                table[i][4].format().styles(tabulate::Style::blink);
-                            }
-                        }
-                    }
-                } else {
-                    auto &row = table.add(v.name, v.avg, "No Reference", v.stddev / v.avg, "N/A");
-                    row[2].format().styles(tabulate::Style::italic);
-                }
-            } else {
-                table.add(v.name, v.avg, v.stddev / v.avg);
-            }
-        }
-        table.format().align(tabulate::Align::center);
-    }
-
-    ProfilerCache() {}
-    ~ProfilerCache()
-    {
-        tabulate::Table table;
-        table.set_title(title);
-        make_summary_table(table);
-
-        /**
-         * output all supported format, and you can catch one or more via
-         *
-         * awk '/BEGIN/{ f = 1; next } /END/{ f = 0 } f' all-formats.txt
-         *
-         */
-        std::cout << "-----BEGIN XTERM TABLE-----" << std::endl;
-        std::cout << table.xterm() << std::endl;
-        std::cout << "-----END XTERM TABLE-----" << std::endl;
-
-        std::cout << "-----BEGIN MARKDOWN TABLE-----" << std::endl;
-        std::cout << table.markdown() << std::endl;
-        std::cout << "-----END MARKDOWN TABLE-----" << std::endl;
-
-        std::cout << "-----BEGIN LATEX TABLE-----" << std::endl;
-        std::cout << table.latex() << std::endl;
-        std::cout << "-----END LATEX TABLE-----" << std::endl;
-    }
-};
-
 class Profiler {
   public:
     Profiler(const std::string &name, std::function<bool(void)> task, size_t repeat = 0, const size_t N = 5)
@@ -260,4 +103,173 @@ class Profiler {
         ProfilerCache::Instance().insert(name, _avg, _stddev);
         std::cout << name << ": avg = " << _avg << ", stddev = " << _stddev << std::endl;
     }
+
+    static void Add(const std::string &name, std::function<bool(void)> task, size_t repeat = 0, const size_t N = 5)
+    {
+        Profiler profiler(name, task, repeat, N);
+    }
+
+    static void AsReference(const std::string &name)
+    {
+        ProfilerCache::Instance().mark_as_ref(name);
+    }
+
+  private:
+    class ProfilerCache {
+      public:
+        static ProfilerCache &Instance()
+        {
+            static ProfilerCache instance;
+            return instance;
+        }
+
+        void insert(const std::string &name, double avg, double stddev)
+        {
+            collections.emplace_back(name, avg, stddev);
+        }
+
+        void mark_as_ref(const std::string &s)
+        {
+            auto it = std::find_if(collections.begin(), collections.end(), [&](const ProfileData &another) {
+                return s == another.name;
+            });
+            if (it != collections.end()) {
+                references.emplace_back(it->name, it->avg, it->stddev);
+                collections.erase(it);
+            } else {
+                std::cout << "not found" << std::endl;
+            }
+        }
+
+        void set_title(std::string title)
+        {
+            this->title = std::move(title);
+        }
+
+      private:
+        struct ProfileData {
+            double avg;
+            double stddev;
+            std::string name;
+            std::vector<std::string> keys;
+            ProfileData(const std::string &name_, double avg_, double stddev_) : avg(avg_), stddev(stddev_), name(name_)
+            {
+                std::regex pattern("([\\w\\-]+)");
+                auto key_end = std::sregex_iterator();
+                auto key_begin = std::sregex_iterator(name_.begin(), name_.end(), pattern);
+
+                for (auto it = key_begin; it != key_end; ++it) {
+                    keys.push_back(it->str());
+                }
+            }
+        };
+        std::string title;
+        std::vector<ProfileData> references;
+        std::vector<ProfileData> collections;
+
+        void make_summary_table(tabulate::Table &table)
+        {
+            if (references.size() == 0) {
+                table.add("description", "average time(nanoseconds)", "coefficient of variation");
+            } else {
+                table.add("description", "average time", "average time(reference)", "coefficient of variation",
+                          "perfermance ratio");
+            }
+
+            int i = 0;
+            for (auto const &v : collections) {
+                if (references.size() != 0) {
+                    auto ref =
+                        std::find_if(references.begin(), references.end(), [&](const ProfileData &another) -> bool {
+                            return v.keys[0] == another.keys[0];
+                        });
+                    if (ref != references.end()) {
+                        double diff = ref->avg / v.avg;
+
+                        std::string name = v.keys[0] + "(";
+                        if (v.keys.size() > 2) {
+                            name += "(";
+                        }
+                        for (size_t _i = 1; _i < v.keys.size(); _i++) {
+                            if (_i == v.keys.size() - 1) {
+                                name += v.keys[_i];
+                            } else {
+                                name += v.keys[_i] + ", ";
+                            }
+                        }
+                        if (v.keys.size() > 2) {
+                            name += ")";
+                        }
+                        name += " vs ";
+                        if (ref->keys.size() > 2) {
+                            name += "(";
+                        }
+                        for (size_t _i = 1; _i < ref->keys.size(); _i++) {
+                            if (_i == ref->keys.size() - 1) {
+                                name += ref->keys[_i];
+                            } else {
+                                name += ref->keys[_i] + ", ";
+                            }
+                        }
+                        if (ref->keys.size() > 2) {
+                            name += ")";
+                        }
+                        name += ")";
+                        table.add(name, v.avg, ref->avg, v.stddev / v.avg, diff);
+
+                        i++;
+                        if (diff >= 1.2) {
+                            table[i][4].format().color(tabulate::Color::green);
+                            if (diff >= 2.0) {
+                                table[i][4].format().styles(tabulate::Style::bold);
+                                if (diff >= 5.0) {
+                                    table[i][4].format().styles(tabulate::Style::blink);
+                                }
+                            }
+                        } else if (diff <= 0.8) {
+                            table[i][4].format().color(tabulate::Color::red);
+                            if (diff <= 0.5) {
+                                table[i][4].format().styles(tabulate::Style::bold);
+                                if (diff <= 0.2) {
+                                    table[i][4].format().styles(tabulate::Style::blink);
+                                }
+                            }
+                        }
+                    } else {
+                        auto &row = table.add(v.name, v.avg, "No Reference", v.stddev / v.avg, "N/A");
+                        row[2].format().styles(tabulate::Style::italic);
+                    }
+                } else {
+                    table.add(v.name, v.avg, v.stddev / v.avg);
+                }
+            }
+            table.format().align(tabulate::Align::center);
+        }
+
+        ProfilerCache() {}
+        ~ProfilerCache()
+        {
+            tabulate::Table table;
+            table.set_title(title);
+            make_summary_table(table);
+
+            /**
+             * output all supported format, and you can catch one or more via
+             *
+             * awk '/BEGIN/{ f = 1; next } /END/{ f = 0 } f' all-formats.txt
+             *
+             */
+            std::cout << "-----BEGIN XTERM TABLE-----" << std::endl;
+            std::cout << table.xterm() << std::endl;
+            std::cout << "-----END XTERM TABLE-----" << std::endl;
+
+            std::cout << "-----BEGIN MARKDOWN TABLE-----" << std::endl;
+            std::cout << table.markdown() << std::endl;
+            std::cout << "-----END MARKDOWN TABLE-----" << std::endl;
+
+            std::cout << "-----BEGIN LATEX TABLE-----" << std::endl;
+            std::cout << table.latex() << std::endl;
+            std::cout << "-----END LATEX TABLE-----" << std::endl;
+        }
+    };
 };
